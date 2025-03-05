679346670ac95cda9ef9e31bd21f8ef8c05bb059
Samuel Nösslböck <samuel.noesslboeck@gmail.com>
2025-03-05 22:12:25 +0100

    adding sudo

--- scripts/penpot_install.sh
+++ scripts/penpot_install.sh
@@ -1,3 +1,3 @@
 # Download from github
-wget https://raw.githubusercontent.com/penpot/penpot/main/docker/images/docker-compose.yaml
-docker compose -p penpot -f docker-compose.yaml up -d
+sudo wget https://raw.githubusercontent.com/penpot/penpot/main/docker/images/docker-compose.yaml
+sudo docker compose -p penpot -f docker-compose.yaml up -d
