6c16f4610201f8af71a54386029a2c1319912618
SamuelNoesslboeck <samuel.noesslboeck@gmail.com>
2026-01-03 14:32:47 +0100

    trying additional directory

--- scripts/samba_install.sh
+++ scripts/samba_install.sh
@@ -17,12 +17,12 @@ sudo smbpasswd -a public
 # Create directories
 echo "| > Creating directories ... "
 
-mkdir -p /home/public/solid_edge_std        # Letter N - "Norm"
+mkdir -p /srv/solid_edge_std            # Letter N - "Norm"
 mkdir -p /home/sy/archive               # Letter S - "Storage"
 mkdir -p /home/sy/labshare              # Letter L - "Labshare"
 
-chown public:public /home/public/solid_edge_std
-chmod -R 777 /home/public/solid_edge_std
+chown public:public /srv/solid_edge_std
+chmod -R 777 /srv/solid_edge_std
 chmod -R 777 /home/sy/archive 
 chmod -R 777 /home/sy/labshare
 
