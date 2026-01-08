1b9ff203df1d40653e009caf60ec0c3aa2c26ad3
Samuel Nösslböck <samuel.noesslboeck@gmail.com>
2026-01-08 22:30:01 +0100

    testing new script

--- scripts/nextcloud_install.sh
+++ scripts/nextcloud_install.sh
@@ -1,13 +1,26 @@
-echo "> [NEXTCLOUD] Starting install ... "
+echo "[NEXTCLOUD] Starting install ... "
 
-sudo docker run \
---init \
---sig-proxy=false \
---name nextcloud-aio-mastercontainer \
---restart always \
---publish 80:80 \
---publish 8080:8080 \
---publish 8443:8443 \
---volume nextcloud_aio_mastercontainer:/mnt/docker-aio-config \
---volume /var/run/docker.sock:/var/run/docker.sock:ro \
-nextcloud/all-in-one:latest
+# PHP Install
+echo "| -> Installing PHP ..."
+
+sudo apt update
+
+sudo apt install software-properties-common
+sudo add-apt-repository ppa:ondrej/php
+sudo apt update
+
+# Modules from requirements
+sudo apt install \
+  php8.3 \
+  php8.3-cli \
+  php8.3-fpm \                  # optional (for Nginx/fastcgi usage)
+  php8.3-curl \
+  php8.3-gd \
+  php8.3-mbstring \
+  php8.3-xml \
+  php8.3-zip \
+  php8.3-common \
+  php8.3-bz2 \
+  php8.3-json \
+  php8.3-opcache \
+  php8.3-pdo_mysql
